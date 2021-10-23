033a60d2aa2833b531ef85e780859330423ec082
Yaroslav Makarov <makarov160393@gmail.com>
2021-10-23 18:41:18 +0300

    fixed content margin top

--- src/pages/Posts/index.jsx
+++ src/pages/Posts/index.jsx
@@ -9,7 +9,7 @@ import './index.scss';
 import { posts } from '../../testData/post.json';
 
 const Posts = () => (
-  <Row xs={1} md={2} lg={3}>
+  <Row style={{ marginTop: '56px' }} xs={1} md={2} lg={3}>
     {posts.map((post) => (
       <Col key={post.id} className="mb-4"><Link className="posts__link" to={`/post/${post.id}`}><Post postId={post.id} /></Link></Col>
     ))}
