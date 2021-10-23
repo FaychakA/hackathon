9aa79f71077aebdccd2225ff3eb907610dfa9bdd
Yaroslav Makarov <makarov160393@gmail.com>
2021-10-23 18:18:43 +0300

    added posts page

--- src/pages/Posts/index.jsx
+++ src/pages/Posts/index.jsx
@@ -1,14 +1,19 @@
 import { React } from 'react';
 import { Link } from 'react-router-dom';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import Post from '../Post';
 
+import './index.scss';
+
+import { posts } from '../../testData/post.json';
+
 const Posts = () => (
-  <div>
-    <h1>Here you can see Posts</h1>
-    <Link to="/post/123">
-      <Post id={123} />
-    </Link>
-  </div>
+  <Row xs={1} md={2} lg={3}>
+    {posts.map((post) => (
+      <Col key={post.id} className="mb-4"><Link className="posts__link" to={`/post/${post.id}`}><Post postId={post.id} /></Link></Col>
+    ))}
+  </Row>
 );
 
 export default Posts;
