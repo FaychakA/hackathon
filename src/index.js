e7805f45ac5c263ffc4f083016029416863d03ac
Yaroslav Makarov <makarov160393@gmail.com>
2021-10-23 12:30:14 +0300

    created reducers states

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from '@reduxjs/toolkit';
 
 import { App } from './App';
 
+import store from './redux/rootReducer';
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
