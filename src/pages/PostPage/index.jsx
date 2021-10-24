414b82ce0f708ad63686b19ec0c5cb5ef168581d
Anatolii Pokhylko <pokhylko@gmail.com>
2021-10-24 17:44:46 +0300

    refactor: comment styles

--- src/pages/PostPage/index.jsx
+++ src/pages/PostPage/index.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, useHistory } from 'react-router';
 import PropTypes from 'prop-types';
-import { Form, Row } from 'react-bootstrap';
+import { Col, Form, Row } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import { useDispatch, useSelector } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
@@ -59,39 +59,47 @@ const PostPage = ({ postId }) => {
       <Post postId={requiredPostId} />
 
       <Row>
-        <Form onSubmit={(e) => handleAddComment(e)}>
-          <h2>Here you can add new comment</h2>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Comment</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Please enter your comment"
-              value={form.content}
-              onChange={(e) => setForm({ ...form, content: e.target.value })}
-            />
-          </Form.Group>
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form>
+        <Col>
+          <Form className="post-page__border post-page__border--form" onSubmit={(e) => handleAddComment(e)}>
+            <h2>Here you can add new comment</h2>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Comment</Form.Label>
+              <Form.Control
+                type="text"
+                as="textarea"
+                placeholder="Please enter your comment"
+                value={form.content}
+                onChange={(e) => setForm({ ...form, content: e.target.value })}
+              />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Col>
       </Row>
 
-      {posts.byId[id]?.comments && Object.keys(posts.byId[id]?.comments).length > 0
-      && Object.keys(posts.byId[id]?.comments).reverse().map((commentId) => (
-        <div className="post-page__comment" key={commentId}>
-          <div className="post-page__user-info">
-            <div>
-              <img src={users.byId[posts.byId[id]?.comments[commentId].userId]?.profilePic} alt="user" />
+      {posts.byId[id]?.comments && Object.keys(posts.byId[id]?.comments).length > 0 && (
+      <div className="post-page__border">
+        {
+          Object.keys(posts.byId[id]?.comments).reverse().map((commentId) => (
+            <div className="post-page__comment" key={commentId}>
+              <div className="post-page__user-info">
+                <div>
+                  <img className="post-page__user-avatar" src={users.byId[posts.byId[id]?.comments[commentId].userId]?.profilePic} alt="user" />
+                </div>
+                <div className="post-page__user-name">
+                  {`Name: ${users.byId[posts.byId[id]?.comments[commentId].userId]?.name}`}
+                </div>
+              </div>
+              <div className="post-page__comment-content">
+                {posts.byId[id]?.comments[commentId]?.content}
+              </div>
             </div>
-            <div className="post-page__user-name">
-              {`Name: ${users.byId[posts.byId[id]?.comments[commentId].userId]?.name}`}
-            </div>
-          </div>
-          <div className="post-page__comment-content">
-            {posts.byId[id]?.comments[commentId]?.content}
-          </div>
-        </div>
-      ))}
+          ))
+}
+      </div>
+      )}
     </div>
   );
 };
