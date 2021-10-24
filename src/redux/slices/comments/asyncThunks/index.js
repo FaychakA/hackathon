0f63b3c447733147fa97e474d34d23143de4cde2
FaychakA <faychak333@gmail.com>
2021-10-24 17:29:42 +0300

    fix add new comment

--- src/redux/slices/comments/asyncThunks/index.js
+++ src/redux/slices/comments/asyncThunks/index.js
@@ -4,7 +4,7 @@ import { writeData } from '../../../../utils/firebase';
 export const createNewComment = createAsyncThunk(
   'comments/createComment',
   async ({ commentData, postId }) => {
-    console.log('commentData, postId', commentData, postId);
     writeData(`posts/${postId}/comments`, commentData.commentId, commentData);
+    return { commentData, postId };
   },
 );
